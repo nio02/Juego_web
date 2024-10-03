2ef0fe6e6234f8c61c791b30a8044afc8aaa07f3
COE9CALIDAD <ingauxiliar.coe9@MHCINGENIEROS.local>
2024-10-03 16:00:28 -0500

    Etiqueta span y nombre dinámico de mascota incluidos

--- js/main.js
+++ js/main.js
@@ -10,19 +10,26 @@ function selectPetPlayer() {
     let inputLangostelvis = document.getElementById("langostelvis")
     let inputTucapalma = document.getElementById("tucapalma")
     let inputPydos = document.getElementById("pydos")
+    let spanPetplayer = document.getElementById("pet-player")
 
     if (inputHipodoge.checked){
         alert("Seleccionaste a Hipodoge")
+        spanPetplayer.innerText = 'Hipodoge'
     } else if (inputCapipepo.checked){
         alert("Seleccionaste a Capipepo")
+        spanPetplayer.innerText = 'Capipepo'
     } else if (inputRatigueya.checked){
         alert("Seleccionaste a Ratigueya")
+        spanPetplayer.innerText = 'Ratigueya'
     } else if (inputLangostelvis.checked){
         alert("Seleccionaste a Langostelvis")
+        spanPetplayer.innerText = 'Langostelvis'
     } else if (inputTucapalma.checked){
         alert("Seleccionaste a Tucapalma")
+        spanPetplayer.innerText = 'Tucapalma'
     } else if (inputPydos.checked){
         alert("Seleccionaste a Pydos")
+        spanPetplayer.innerText = 'Pydos'
     } else {
         alert("Selecciona una mascota por favor")
     }
